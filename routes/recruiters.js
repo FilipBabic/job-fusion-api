e6fc47774ecca9b2afe67e4bb808d9d81fe4f965
Filip Babic <filip1986babic@gmail.com>
2024-09-12 16:34:34 +0200

    Completed CRUD operations for jobs and organizations

--- routes/recruiters.js
+++ routes/recruiters.js
@@ -11,6 +11,37 @@ import checkIsRecruiter from "../middleware/auth/check-is-recruiter.js";
 
 const router = express.Router();
 
+// @desc Fetch organization for recruiter - route.
+// @route GET api/recruiters/organization
+
+router.get("/organization", authenticateUser, checkIsRecruiter, async (req, res) => {
+  const recruiterID = req.user.uid;
+  try {
+    const recruiterRef = db.collection("recruiters").doc(recruiterID);
+    const recruiterDoc = await recruiterRef.get();
+
+    if (!recruiterDoc.exists) {
+      return res.status(404).json({ error: "Recruiter not found" });
+    }
+
+    const organizationRef = recruiterDoc.data().organization;
+
+    if (!organizationRef) {
+      return res.status(404).json({ message: "Organization not found" });
+    }
+    const organizationDoc = await organizationRef.get();
+
+    if (!organizationDoc.exists) {
+      return res.status(404).json({ message: "Organization doc not found" });
+    }
+    const organizationData = organizationDoc.data();
+
+    res.status(200).json(organizationData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch organization" });
+  }
+});
+
 // @desc Create organization - route. Recruiter can create one organization only.
 // @route POST api/recruiters/organization
 
@@ -79,7 +110,7 @@ router.post(
   }
 );
 
-// @desc Update organization - route. Only recruiter can update his organization info.
+// @desc Update organization - route. Only logged recruiter can update his organization info.
 // @route PATCH api/recruiters/organization
 
 router.patch(
@@ -148,90 +179,118 @@ router.patch(
   }
 );
 
+// @desc Delete organization and recruiter reference - route. Only logged recruiter can delete his organization.
+// @route DELETE api/recruiters/organization
+// TODO add deleting recruiter jobs after deleting organization or remove this route
+
+router.delete("/organization", authenticateUser, checkIsRecruiter, async (req, res) => {
+  const recruiterID = req.user.uid;
+  try {
+    const recruiterRef = db.collection("recruiters").doc(recruiterID);
+    const recruiterDoc = await recruiterRef.get();
+
+    if (!recruiterDoc.exists) {
+      return res.status(404).json({ error: "Recruiter not found" });
+    }
+
+    const organizationRef = recruiterDoc.data().organization;
+
+    if (!organizationRef) {
+      return res.status(404).json({ message: "Organization not found" });
+    }
+
+    const organizationDoc = await organizationRef.get();
+
+    if (!organizationDoc) {
+      return res.status(404).json({ message: "Organization doc not found" });
+    }
+
+    await organizationRef.delete();
+    await recruiterRef.update({ organization: firebase.firestore.FieldValue.delete() });
+    return res.status(200).json({ msg: "Organization successfully deleted" });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to delete organization" });
+  }
+});
+
 // @desc Post a job - route. Recruiter can create many jobs for his organization.
 // @route POST api/recruiters/post-job
 // @TODO: Change typeof starts and expires date, from string to timestamp
 
-router.post(
-  "/post-job",
-  authenticateUser,
-  checkIsRecruiter,
-  validateJobPosting,
-  async (req, res) => {
-    // Checking if there is an error in body validation
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const errorMessage = errors
-        .array()
-        .slice(0, 20)
-        .map((error) => error.msg)
-        .join(", ");
-      return res.status(400).json({ error: errorMessage });
-    }
-
-    const { title, location, starts, expires, skills, jobDescriptions, salary, type } = req.body;
-    const recruiterId = req.user.uid;
-
-    try {
-      const recruiterRef = db.collection("recruiters").doc(recruiterId);
-      const recruiterDoc = await recruiterRef.get();
-
-      if (!recruiterDoc.exists) {
-        return res.status(404).json({ error: "Recruiter not found" });
-      }
-
-      const recruiterData = recruiterDoc.data();
-
-      if (!recruiterData || !recruiterData.organization) {
-        return res.status(401).json({
-          error: "Organization not found. Please create organization first",
-        });
-      }
-
-      const organizationRef = recruiterData.organization;
-      const organizationDoc = await organizationRef.get();
-      const organizationData = organizationDoc.data();
-
-      if (!organizationData || !organizationData.name) {
-        return res.status(401).json({ error: "Organization data not found" });
-      }
-
-      const jobRef = db.collection("job_postings").doc();
-      await jobRef.set({
-        title,
-        location,
-        starts,
-        expires,
-        skills,
-        jobDescriptions,
-        applicants: [],
-        type: type || "",
-        salary: salary || "",
-        organizationName: organizationData.name,
-        organizationLogo: organizationData.logo || "no_logo.png",
-        organizationRef: organizationRef,
-        recruiterRef: recruiterRef,
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
-      });
-
-      await recruiterRef.update({
-        postedJobs: firebase.firestore.FieldValue.arrayUnion(jobRef), // Add job reference to postedJobs array
-      });
-
-      return res.status(201).json({ msg: "Job created successfully" });
-    } catch (error) {
-      return res.status(500).json({ error: "Failed to post a job" });
-    }
+router.post("/job", authenticateUser, checkIsRecruiter, validateJobPosting, async (req, res) => {
+  // Checking if there is an error in body validation
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errorMessage = errors
+      .array()
+      .slice(0, 20)
+      .map((error) => error.msg)
+      .join(", ");
+    return res.status(400).json({ error: errorMessage });
   }
-);
+
+  const { title, location, starts, expires, skills, jobDescriptions, salary, type } = req.body;
+  const recruiterID = req.user.uid;
+
+  try {
+    const recruiterRef = db.collection("recruiters").doc(recruiterID);
+    const recruiterDoc = await recruiterRef.get();
+
+    if (!recruiterDoc.exists) {
+      return res.status(404).json({ error: "Recruiter not found" });
+    }
+
+    const recruiterData = recruiterDoc.data();
+
+    if (!recruiterData || !recruiterData.organization) {
+      return res.status(401).json({
+        error: "Organization not found. Please create organization first",
+      });
+    }
+
+    const organizationRef = recruiterData.organization;
+    const organizationDoc = await organizationRef.get();
+    const organizationData = organizationDoc.data();
+
+    if (!organizationData || !organizationData.name) {
+      return res.status(401).json({ error: "Organization data not found" });
+    }
+
+    const jobRef = db.collection("job_postings").doc();
+    await jobRef.set({
+      title,
+      location,
+      starts,
+      expires,
+      skills,
+      jobDescriptions,
+      applicants: [],
+      type: type || "",
+      salary: salary || "",
+      organizationName: organizationData.name,
+      organizationLogo: organizationData.logo || "no_logo.png",
+      organizationRef: organizationRef,
+      recruiterRef: recruiterRef,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    await recruiterRef.update({
+      postedJobs: firebase.firestore.FieldValue.arrayUnion(jobRef), // Add job reference to postedJobs array
+    });
+
+    return res.status(201).json({ msg: "Job created successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to post a job" });
+  }
+});
 
 // @desc Update a job - route. Recruiter can only upgrade jobs that he has published
 // @route PATCH api/recruiters/update-job/:id
 // @TODO: fix validation for array of jobDescriptions
 
 router.patch(
-  "/update-job/:id",
+  "/job/:id",
   authenticateUser,
   checkIsRecruiter,
   validateJobUpdating,
@@ -305,10 +364,10 @@ router.patch(
 // @route GET api/recruiters/jobs
 
 router.get("/jobs", authenticateUser, checkIsRecruiter, async (req, res) => {
-  const recruiterId = req.user.uid;
+  const recruiterID = req.user.uid;
 
   try {
-    const recruiterRef = db.collection("recruiters").doc(recruiterId);
+    const recruiterRef = db.collection("recruiters").doc(recruiterID);
     const recruiterDoc = await recruiterRef.get();
 
     if (!recruiterDoc.exists) {
@@ -336,6 +395,51 @@ router.get("/jobs", authenticateUser, checkIsRecruiter, async (req, res) => {
     return res.status(200).json(jobPostings);
   } catch (error) {
     return res.status(500).json({ error: "Failed to fetch jobs" });
+  }
+});
+
+// @desc Delete a job for logged recruiter - route. Recruiter can delete one jobs per call.
+// @route DELETE api/recruiters/job/:id
+// TODO Add validation if the recruiter has no posted jobs
+
+router.delete("/job/:id", authenticateUser, checkIsRecruiter, async (req, res) => {
+  const { id } = req.params;
+  const recruiterID = req.user.uid;
+  const { postedJobs } = req.user;
+
+  try {
+    // Get reference to the job that the recruiter wants to update
+    const jobRef = db.collection("job_postings").doc(id);
+    const jobDoc = await jobRef.get();
+
+    if (!jobDoc.exists) {
+      return res.status(404).json({ error: " Job not found" });
+    }
+    // Check if the jobRef exists in the recruiter's postedJobs (array of references)
+    const jobOwnedByRecruiter = postedJobs.some(
+      (postedJobRef) => postedJobRef.path === jobRef.path
+    );
+
+    // If the job is not owned by the recruiter, deny access
+    if (!jobOwnedByRecruiter) {
+      return res.status(403).json({
+        error: "You are not authorized to delete this job.",
+      });
+    }
+
+    // Get the recruiter document reference
+    const recruiterRef = db.collection("recruiters").doc(recruiterID);
+
+    // Update the recruiter document by removing the job reference from the postedJobs array
+    await recruiterRef.update({
+      postedJobs: firebase.firestore.FieldValue.arrayRemove(jobRef), // Remove the job reference from the array
+    });
+
+    await jobRef.delete();
+
+    return res.status(200).json({ msg: "Successfully deleted job posting" });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to delete job posting" });
   }
 });
 export default router;
